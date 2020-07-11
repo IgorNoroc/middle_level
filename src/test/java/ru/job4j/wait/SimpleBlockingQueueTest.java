2c4667c992620cd3b3a9b873c4665dff64ad612c
Igor <workuglis@gmail.com>
2020-07-11 16:12:32 +0300

    3. Junit тест для блокирующей очереди. [#318305]

--- src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertThat;
 
 public class SimpleBlockingQueueTest {
     private final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+    private final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
 
     @Test
     public void whenHave2Threads() throws InterruptedException {
@@ -36,8 +37,6 @@ public class SimpleBlockingQueueTest {
 
     @Test
     public void whenFetchAllThenGetIt() throws InterruptedException {
-        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
-        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
         Thread producer = new Thread(
                 () -> {
                     IntStream.range(0, 5).forEach(
@@ -62,5 +61,48 @@ public class SimpleBlockingQueueTest {
         consumer.interrupt();
         consumer.join();
         assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+    @Test
+    public void whenHave3ThreadsAndCollectToList() throws InterruptedException {
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 10).forEach(
+                        queue::offer)
+        );
+        producer.start();
+        Thread consumer1 = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        Thread consumer2 = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer1.start();
+        consumer2.start();
+        producer.join();
+        consumer1.interrupt();
+        consumer2.interrupt();
+        consumer1.join();
+        consumer2.join();
+        assertThat(buffer.containsAll(Arrays.asList(
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+                )),
+                is(true)
+        );
     }
 }
