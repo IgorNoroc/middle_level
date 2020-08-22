3feb8c872ce8fe78b19f5fe8c3b5292cc4aad9dd
Igor <workuglis@gmail.com>
2020-08-22 15:18:02 +0300

    0. Что такое DI? [#318277]

---
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -0,0 +1,16 @@
+package ru.job4j.di;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleInput {
+    private List<String> list = new ArrayList<>();
+
+    public void addToList(String value) {
+        list.add(value);
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+}
